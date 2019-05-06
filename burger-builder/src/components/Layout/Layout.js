f1ce5dfa8fa5941a74c5982da3e40362baf92b6e
treyarnold <arnold.trey80@gmail.com>
2019-05-06 16:12:58 -0500

    created burgerbuilder show page container and css module

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
+
+console.log(classes);
 
 const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
