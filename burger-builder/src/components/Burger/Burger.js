58eecef5390903680dc48a0d0f342984bb23f83f
Trey Arnold <arnold.trey80@gmail.com>
2019-06-06 16:03:05 -0500

    adding state to burger container

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = props => {
+const burger = (props) => {
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
