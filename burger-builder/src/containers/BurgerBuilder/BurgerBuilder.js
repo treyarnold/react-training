1b95851ab0c255ca9895c86acce2bcaf9400072f
Trey Arnold <arnold.trey80@gmail.com>
2019-06-11 13:33:48 -0500

    burger component now takes props to render

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,20 +5,20 @@ import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
   }
 }
 
-export default BurgerBuildera
+export default BurgerBuilder
