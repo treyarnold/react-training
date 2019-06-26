1045716ef4d36c1ff4625de013c810c19087dd5b
Trey Arnold <arnold.trey80@gmail.com>
2019-06-26 17:27:28 -0500

    added interceptors to axios

--- blog-spa-routing/src/containers/Blog/Blog.js
+++ blog-spa-routing/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -7,42 +7,49 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-  state= {
+  state = {
     posts: [],
     selectedPostID: null,
+    error: false
   }
-  componentDidMount () {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      const posts = response.data.slice(0,4);
+  componentDidMount() {
+    axios.get("/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
       const updatedPosts = posts.map((post) => {
         return {
           ...post,
           author: 'Trey'
         }
-      });
-      this.setState({posts: updatedPosts})
+      })
+      this.setState({ posts: updatedPosts });
+    }).catch((error) => {
+      console.log(error);
+      this.setState({ error: true });
     })
   }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostID: id})
+    this.setState({ selectedPostID: id })
   }
 
   render() {
-    const posts = this.state.posts.map((post) => {
-      return <Post 
-        key={post.id} 
-        title={post.title} 
-        author={post.author}
-        clicked={() => this.postSelectedHandler(post.id)}/>;
-    })
+    let posts = <p>Something went wrong!</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
+        return <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)} />;
+      })
+    }
     return (
       <div>
         <section className="Posts">
           {posts}
         </section>
         <section>
-          <FullPost id={this.state.selectedPostID}/>
+          <FullPost id={this.state.selectedPostID} />
         </section>
         <section>
           <NewPost />
